b3cc4889d97c64ba04746fd47fc7175328b62433
Winfred Sunga <winfredsunga01@gmail.com>
2017-11-01 17:28:09 -0700

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
-var mysql = require("mysql");
-
+var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = msql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "password",
-    database : "burgers_db"
+    host : 'localhost',
+    user : 'root',
+    password : 'password',
+    database : 'burgers_db'
   });
 }
 
